1b4e9efa61219db3296e025acd25f3b5c08aa5cf
Aakash Basnet <iamaakashbasnet@gmail.com>
2025-01-21 08:27:08 +0545

    feat: redirect auth user to dashboard from home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,11 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        if (User.Identity != null && User.Identity.IsAuthenticated)
+        {
+            return RedirectToAction("Dashboard", "Account");
+        }
+
         return View();
     }
 }
